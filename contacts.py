1aef3340cf8a7c24cb90099901cb75b0d858f690
Narayan Sajeev <narayansajeev1@gmail.com>
2024-07-04 18:13:14 -0400

    Only print new contacts

--- contacts.py
+++ contacts.py
@@ -27,21 +27,8 @@ for row in df.iterrows():
     # Append old contacts to list
     old_lst.append([val.Name, str(val.Phone)])
 
-print('Name\tPhone')
-
-# Sort old contacts by last name
-old_lst = sorted(old_lst, key=lambda x: x[0].split()[-1])
-
-for old_row in old_lst:
-    print('\t'.join(old_row))
-
 # Remove old contacts from new list
 new_lst = [x for x in new_lst if x not in old_lst]
-
-print('\n\n')
-
-if len(new_lst) < 1:
-    print('No new contacts found.')
 
 for new_row in new_lst:
     print('\t'.join(new_row))
