b86762f1f3726208b3dbd8fdbd330bd73ac9d202
Narayan Sajeev <narayansajeev1@gmail.com>
2024-07-17 16:39:20 -0400

    Add relation column, refactor code

--- contacts.py
+++ contacts.py
@@ -1,34 +1,39 @@
 import pandas as pd
 import vobject
 
-new_cons = 'contacts.vcf'
-new_lst = []
+saved_cons = 'contacts.vcf'
+saved_lst = []
 
-old_cons = 'Contacts.xlsx'
-old_lst = []
+xl_cons = 'Contacts.xlsx'
+xl_lst = []
 
-with open(new_cons, 'r') as file:
+with open(saved_cons, 'r') as file:
     # Loop through contacts in vcf file
     for contact in vobject.readComponents(file.read()):
         name = ' '.join([n.capitalize() for n in contact.fn.value.split()])
+        rel = ''
+        try:
+            rel = contact.org.value[0]
+        except:
+            pass
         # Retrieve phone number
         try:
             phone = contact.tel.value.replace(' ', '').replace('+1', '').replace('+', '').replace(')', '')
             phone = phone.replace('(', '').replace('-', '')
-            new_lst.append([name, phone])
+            saved_lst.append([name, phone, rel])
         except:
             pass
 
-# Read old contacts from Excel file
-df = pd.read_excel(old_cons).fillna('')
+# Read contacts from Excel file
+df = pd.read_excel(xl_cons).fillna('')
 
 for row in df.iterrows():
     val = row[1]
-    # Append old contacts to list
-    old_lst.append([val.Name, str(val.Phone)])
+    # Append contacts to list
+    xl_lst.append([val.Name, str(val.Phone), val.Relation])
 
-# Remove old contacts from new list
-new_lst = [x for x in new_lst if x not in old_lst]
+# Remove old contacts from list
+saved_lst = [x for x in saved_lst if x not in xl_lst]
 
-for new_row in new_lst:
-    print('\t'.join(new_row))
+for row in saved_lst:
+    print('\t'.join(row))
